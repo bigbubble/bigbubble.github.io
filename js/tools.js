e98e6e442ff484c9a55edcc8dbf88aff420403e8
pengbo <pengbowo@126.com>
2020-04-17 17:25:24 +0800

    message converter

--- js/tools.js
+++ js/tools.js
@@ -1,122 +1,126 @@
 var allLinks = [
   {
-    "title": "构建二叉树",
-    "href": "html\\algorithm\\build-binary-tree.html"
-  },
+    "href": "html/tools/ws-test.html", 
+    "title": "websokect test"
+  }, 
   {
-    "title": "打乱数组排列顺序",
-    "href": "html\\algorithm\\disrupt-array-order.html"
-  },
+    "href": "html/tools/put-text-into-png.html", 
+    "title": "将文字信息保存到PNG图片中"
+  }, 
   {
-    "title": "二个数组分别分成小数组,使A中小数组大小与B中小数组大小的乘积小于一个值",
-    "href": "html\\algorithm\\divide-two-arrays-by-condition.html"
-  },
+    "href": "html/tools/tui-image-editor.html", 
+    "title": "图片编辑"
+  }, 
   {
-    "title": "二叉树显示-echarts",
-    "href": "html\\algorithm\\echarts-binary-tree.html"
-  },
+    "href": "html/tools/js-is-prime.html", 
+    "title": "判断一个数是否是质数"
+  }, 
   {
-    "title": "JS LRU Cache",
-    "href": "html\\algorithm\\js-lru.html"
-  },
+    "href": "html/tools/distance-of-two-points-on-sphere.html", 
+    "title": "两点间距离"
+  }, 
   {
-    "title": "SQL:查询条件有两个in,分组拼接查询,分页",
-    "href": "html\\algorithm\\query-from-database-by-group.html"
-  },
+    "href": "html/tools/js-delimiter-transfer.html", 
+    "title": "分隔符转换"
+  }, 
   {
-    "title": "SQL:查询条件有两个in,分组拼接查询,分页-2",
-    "href": "html\\algorithm\\query-from-database-by-group2.html"
-  },
+    "href": "html/tools/calculate_age_and_month.html", 
+    "title": "根据日期计算年龄和月份"
+  }, 
   {
-    "title": "存储大量文件，让每个文件夹下的文件或文件夹数量不大于1024",
-    "href": "html\\algorithm\\store-files-in-file-sytem-less-than-1024-each.html"
-  },
+    "href": "html/tools/argument-signatures-by-md5.html", 
+    "title": "对参数进行MD5签名"
+  }, 
   {
-    "title": "存储大量文件，让每个文件夹下的文件或文件夹数量不大于1024",
-    "href": "html\\diary\\what-happened-today-parser.html"
-  },
+    "href": "html/read/jul-to-slf4j-in-webapp.html", 
+    "title": "jul-to-slf4j使用"
+  }, 
   {
-    "title": "WHAT HAPPENED TODAY",
-    "href": "html\\diary\\what-happened-today.html"
-  },
+    "href": "html/read/design-patterns.html", 
+    "title": "设计模式"
+  }, 
   {
-    "title": "java SimpleDateFormat和FastDateFormat格式化yyMMdd日期问题",
-    "href": "html\\function_test\\java-dateformat-of-yymmdd.html"
-  },
+    "href": "html/read/building-nginx-from-sources-translate.html", 
+    "title": "从源码构建nginx"
+  }, 
   {
-    "title": "java 8泛型问题",
-    "href": "html\\function_test\\java-generics.html"
-  },
+    "href": "html/read/startup-You-have-no-idea-how-much-that-costs.html", 
+    "title": "创业公司：你不知道花多少时间解决问题"
+  }, 
   {
-    "title": "JavaScript串口测试",
-    "href": "html\\function_test\\MScomm32.html"
-  },
+    "href": "html/read/double-checked-locking.html", 
+    "title": "双锁检测"
+  }, 
   {
-    "title": "SpringWeb RestTemplate HttpMessageConverter问题",
-    "href": "html\\function_test\\springweb-resttemplate-httpmessageconverter.html"
-  },
+    "href": "html/read/understanding_Java_Lambdas_translation.html", 
+    "title": "理解JAVA Lambda表达式"
+  }, 
   {
-    "title": "关于数据库连接池大小",
-    "href": "html\\read\\about-pool-sizing.html"
-  },
+    "href": "html/read/about-pool-sizing.html", 
+    "title": "关于数据库连接池大小"
+  }, 
   {
-    "title": "从源码构建nginx",
-    "href": "html\\read\\building-nginx-from-sources-translate.html"
-  },
+    "href": "html/read/how-computer-bootup.html", 
+    "title": "计算机是如何启动的？"
+  }, 
   {
-    "title": "设计模式",
-    "href": "html\\read\\design-patterns.html"
-  },
+    "href": "html/algorithm/js-lru.html", 
+    "title": "JS LRU Cache"
+  }, 
   {
-    "title": "双锁检测",
-    "href": "html\\read\\double-checked-locking.html"
-  },
+    "href": "html/algorithm/query-from-database-by-group2.html", 
+    "title": "SQL:查询条件有两个in,分组拼接查询,分页-2"
+  }, 
   {
-    "title": "计算机是如何启动的？",
-    "href": "html\\read\\how-computer-bootup.html"
-  },
+    "href": "html/algorithm/store-files-in-file-sytem-less-than-1024-each.html", 
+    "title": "存储大量文件，让每个文件夹下的文件或文件夹数量不大于1024"
+  }, 
   {
-    "title": "jul-to-slf4j使用",
-    "href": "html\\read\\jul-to-slf4j-in-webapp.html"
-  },
+    "href": "html/algorithm/echarts-binary-tree.html", 
+    "title": "二叉树显示-echarts"
+  }, 
   {
-    "title": "创业公司：你不知道花多少时间解决问题",
-    "href": "html\\read\\startup-You-have-no-idea-how-much-that-costs.html"
-  },
+    "href": "html/algorithm/disrupt-array-order.html", 
+    "title": "打乱数组排列顺序"
+  }, 
   {
-    "title": "理解JAVA Lambda表达式",
-    "href": "html\\read\\understanding_Java_Lambdas_translation.html"
-  },
+    "href": "html/algorithm/build-binary-tree.html", 
+    "title": "构建二叉树"
+  }, 
   {
-    "title": "对参数进行MD5签名",
-    "href": "html\\tools\\argument-signatures-by-md5.html"
-  },
+    "href": "html/algorithm/query-from-database-by-group.html", 
+    "title": "SQL:查询条件有两个in,分组拼接查询,分页"
+  }, 
   {
-    "title": "根据日期计算年龄和月份",
-    "href": "html\\tools\\calculate_age_and_month.html"
-  },
+    "href": "html/algorithm/divide-two-arrays-by-condition.html", 
+    "title": "二个数组分别分成小数组,使A中小数组大小与B中小数组大小的乘积小于一个值"
+  }, 
   {
-    "title": "两点间距离",
-    "href": "html\\tools\\distance-of-two-points-on-sphere.html"
-  },
+    "href": "html/function_test/springweb-resttemplate-httpmessageconverter.html", 
+    "title": "SpringWeb RestTemplate HttpMessageConverter问题"
+  }, 
   {
-    "title": "分隔符转换",
-    "href": "html\\tools\\js-delimiter-transfer.html"
-  },
+    "href": "html/function_test/MScomm32.html", 
+    "title": "JavaScript串口测试"
+  }, 
   {
-    "title": "判断一个数是否是质数",
-    "href": "html\\tools\\js-is-prime.html"
-  },
+    "href": "html/function_test/java-generics.html", 
+    "title": "java 8泛型问题"
+  }, 
   {
-    "title": "将文字信息保存到PNG图片中",
-    "href": "html\\tools\\put-text-into-png.html"
-  },
+    "href": "html/function_test/fastjsonhttpmessageconverter-supported-medias-problem.html", 
+    "title": "SpringWeb FastJsonHttpMessageConverter supportedMediaTypes 配置问题"
+  }, 
   {
-    "title": "图片编辑",
-    "href": "html\\tools\\tui-image-editor.html"
-  },
+    "href": "html/function_test/java-dateformat-of-yymmdd.html", 
+    "title": "java SimpleDateFormat和FastDateFormat格式化yyMMdd日期问题"
+  }, 
   {
-    "title": "websokect test",
-    "href": "html\\tools\\ws-test.html"
+    "href": "html/diary/what-happened-today-parser.html", 
+    "title": "java SimpleDateFormat和FastDateFormat格式化yyMMdd日期问题"
+  }, 
+  {
+    "href": "html/diary/what-happened-today.html", 
+    "title": "WHAT HAPPENED TODAY"
   }
 ]
